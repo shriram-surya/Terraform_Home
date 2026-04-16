9f372f105bbb3ac917556f456da790f8aac073b6
shriram-surya <sdsuryawanshi0420@gmail.com>
2026-04-16 02:25:11 +0000

    updated files

--- main.tf
+++ main.tf
@@ -4,3 +4,10 @@ resource "local_file" "sawalsur" {
 	filename = "/mnt/farm.html"
 	content = "hello Gavkari"
 }
+resource "local_file" "my" {
+
+  filename = "/mnt/friends.html"
+  content = "hello mitranno"
+}
+
+
